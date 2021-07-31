6a4b64c5ceb88ad5b1c431e78c6d9c81c2f1f1a7
Oster.Huang <oster.huang@gramltd.com>
2021-07-31 21:10:32 +0800

    Init Project with Tailwind / VantUI

---
+++ src/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1 class="text-5xl">This is home</h1>
+    <VantDemo />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import VantDemo from '@/components/vantDemo'
+
+export default {
+  components: {
+    VantDemo
+  },
+  setup() {
+    const rateNo = ref(3)
+    return { rateNo }
+  },
+}
+</script>
+
+
+<style lang="stylus" scoped>
+.home
+  padding 12px
+</style>
---
+++ src/components/vantDemo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-center">
+    <VantIcon name="star-o" />
+    <VantIcon name="cart-circle-o" />
+    <VantRate v-model="rateNo" />
+  </div>
+</template>
+
+<script>
+import { Icon, Rate } from 'vant'
+
+export default {
+  components: {
+    VantIcon: Icon,
+    VantRate: Rate
+  },
+  setup() {
+    
+  },
+}
+</script>
